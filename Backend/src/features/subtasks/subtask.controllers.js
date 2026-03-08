ffc88082b81347190004de7d0517945a06f36b12
soumishjain <jainmashu13@gmail.com>
2026-03-08 15:37:09 +0530

    subtaskControllers done

--- Backend/src/features/subtasks/subtask.controllers.js
+++ Backend/src/features/subtasks/subtask.controllers.js
@@ -1,5 +1,6 @@
-import subtaskModel from "../../models/subtask.models"
-import taskModel from "../../models/task.models"
+import departmentMemberModel from "../../models/departmentMember.models.js"
+import subtaskModel from "../../models/subtask.models.js"
+import taskModel from "../../models/task.models.js"
 
 export async function getSubtasksOfTasks(req,res){
     try{
@@ -96,8 +97,6 @@ export async function claimSubtask(req,res) {
         })
     }    
 
-
-
     subtask.assignedTo = userId
     subtask.claimedAt = new Date()
 
@@ -117,9 +116,87 @@ export async function claimSubtask(req,res) {
 }
 
 export async function completeSubtask(req,res) {
-    
+    try{
+        const {subtaskId} = req.params
+    const userId = req.userId
+
+    const subtask = await subtaskModel.findById(subtaskId)
+
+    if(!subtask){
+        return res.status(404).json({
+            message : "Subtask not found"
+        })
+    }
+
+    if(!subtask.assignedTo || subtask.assignedTo?.toString() !== userId){
+        return res.status(403).json({
+            message : "You did not claim this subtask"
+        })
+    }
+
+    if(subtask.status === 'completed') {
+        return res.status(400).json({
+            message : "Subtask already completed"
+        })
+    }
+
+    const task = await taskModel.findById(subtask.taskId)
+    if(!task){
+        return res.status(404).json({
+            message : "Task not found"
+        })
+    }
+
+    subtask.status = 'completed'
+    subtask.completedAt = new Date()
+    subtask.completedBy = userId
+
+    await subtask.save()
+
+    const progress = (task.completedSubtasks / task.totalSubtasks ) * 90;
+
+    task.progress = Math.floor(progress)
+    task.completedSubtasks += 1;
+
+    if(task.completedSubtasks === task.totalSubtasks){
+        task.status = 'awaiting-approval'
+        task.progress = 90
+    }
+
+    await task.save()
+
+    return res.status(200).json({
+        message : "Subtask completed Successfully"
+    })
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({
+            message : "Internal Server Error"
+        })
+    }
+
 }
 
 export async function getMyPendingSubtasks(req,res) {
+    try{
+        const userId = req.userId
+
+    const subtasks = await subtaskModel.find({
+        assignedTo : userId,
+        status : 'pending'
+    })
+    .populate("taskId" , "title priority deadline")
+
+    return res.status(200).json({
+        message : "Pending subtasks fetched",
+        total : subtasks.length,
+        subtasks
+    })
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({
+            message : "Internal Server Error"
+        })
+    }
 
 }
