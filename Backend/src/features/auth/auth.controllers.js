b7ecebc85a7017fbfefe1a4c5ca7c7711cc0df3b
soumishjain <jainmashu13@gmail.com>
2026-03-27 17:19:11 +0530

    temp comment

--- Backend/src/features/auth/auth.controllers.js
+++ Backend/src/features/auth/auth.controllers.js
@@ -70,15 +70,15 @@ export async function registerUser(req,res){
 
   const verificationLink = `${process.env.VITE_URL}/api/auth/verify-email?token=${rawToken}`
 
-  await sendEmail({
-    to: user.email,
-    subject: "Verify Your Email",
-    html : `
-        <h2>Email Verification</h2>
-        <p>Click the button below to verify your email:</p>
-        <a href="${verificationLink}">Verify Email</a>
-      `
-  })
+  // await sendEmail({
+  //   to: user.email,
+  //   subject: "Verify Your Email",
+  //   html : `
+  //       <h2>Email Verification</h2>
+  //       <p>Click the button below to verify your email:</p>
+  //       <a href="${verificationLink}">Verify Email</a>
+  //     `
+  // })
 
 
     res.status(201).json({
