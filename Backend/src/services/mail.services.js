3cee1deb970e6c91e95c461561a2aabe8e5fcabf
soumishjain <jainmashu13@gmail.com>
2026-03-27 17:29:31 +0530

    resend used for email

--- Backend/src/services/mail.services.js
+++ Backend/src/services/mail.services.js
@@ -1,28 +1,18 @@
-import nodemailer from 'nodemailer'
+import { Resend } from "resend";
 
-// Create a transporter using SMTP
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async ({to , subject , html}) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"CoWorkingSpace" <${process.env.SENDER_EMAIL}>`, // sender address
-      to, // list of recipients
-      subject, // subject line
-      html, // HTML body
-    });
-
-    console.log("Message sent: ", info.messageId);
-  } catch (error) {
-    console.error("Email sending failed:", error);
-    throw new Error("Email could not be sent");
+    const response = await resend.emails.send({
+      from: "CoWorkingSpace <onboarding@resend.dev>",
+      to,
+      subject,
+      html,
+    })
+    console.log("Email Sent: ",  response)
+  }catch(err){
+    console.error("Email sending failed: ", err)
+    throw new Error("Emial could not be sent")
   }
 }
